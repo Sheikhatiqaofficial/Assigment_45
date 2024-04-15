08ca42da605f181fb13903c22180ced3f9018c9c
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 06:31:00 +0500

    Add files via upload

---
+++ 6.STRIPPING NAMES/strippingnames.js
@@ -0,0 +1,12 @@
+"use strict";
+const myName = "   Atiqa   ";
+// Before stripping white spaces:
+console.log(myName);
+// After stripping white spaces:
+console.log(myName.trim());
+// "\t" used for tab/spaces:
+const myname = "\tAtiqa";
+console.log(myname);
+// \n used for new line:
+const Myname = "\nAtiqa";
+console.log(Myname);
---
+++ 6.STRIPPING NAMES/strippingnames.ts
@@ -0,0 +1,18 @@
+const myName: string ="   Atiqa   ";
+
+// Before stripping white spaces:
+console.log(myName);
+
+
+// After stripping white spaces:
+console.log(myName.trim())
+
+
+// "\t" used for tab/spaces:
+const myname: string ="\tAtiqa"
+console.log(myname);
+
+
+// \n used for new line:
+const Myname: string ="\nAtiqa"
+console.log(Myname)
