06740378fd57f3ce6f68f15020f3418e5dbeeba2
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 07:07:39 +0500

    Add files via upload

---
+++ 20.PLACES2/places2.js
@@ -0,0 +1,4 @@
+"use strict";
+let program = ["typescript", "football", "robots", "poetry", "coffee"];
+// Making list of these items:
+program.forEach((ib) => { console.log(ib); });
---
+++ 20.PLACES2/places2.ts
@@ -0,0 +1,4 @@
+let program : string [] = ["typescript", "football", "robots", "poetry", "coffee"];
+
+// Making list of these items:
+program.forEach((ib)=>{console.log(ib);})
