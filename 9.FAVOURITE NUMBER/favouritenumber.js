019c5db88ed937bed6d13e4748071b34a7f95d6a
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 06:34:02 +0500

    Add files via upload

---
+++ 9.FAVOURITE NUMBER/favouritenumber.js
@@ -0,0 +1,4 @@
+"use strict";
+const favouriteNumber = 1;
+const message = `My favourite number is ${favouriteNumber}`;
+console.log(message);
---
+++ 9.FAVOURITE NUMBER/favouritenumber.ts
@@ -0,0 +1,5 @@
+const favouriteNumber: number = 1;
+
+const message: string =`My favourite number is ${favouriteNumber}`;
+
+console.log(message)
