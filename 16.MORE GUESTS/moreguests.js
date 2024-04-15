c0769a3c8fa58fe8a42b36f9dd14724a67999f4c
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 07:04:09 +0500

    Add files via upload

---
+++ 16.MORE GUESTS/moreguests.js
@@ -0,0 +1,19 @@
+"use strict";
+const myFriends = ["Afifa", "Ayesha", "Umar"];
+console.log(myFriends);
+console.log(`I want to invite 3 more friends because i found bigger dinner table\n`);
+// ADDING THREE MORE FRIENDS:
+// adding at beggining:
+myFriends.unshift("Bushra");
+// ["Bushra", Afifa", "Ayesha", "Umar"]
+console.log(myFriends);
+// adding at middle:
+myFriends.splice(2, 0, "Adeel");
+console.log(myFriends);
+// adding at end:
+myFriends.push("Atiqa");
+console.log(myFriends);
+// print all friends along with message:
+for (let i = 0; i < myFriends.length; i++) {
+    console.log(`Mr/Miss. ${myFriends[i]}! you are invited for dinner at my villa.`);
+}
