ad28b24915d18bb08db789c28d3891055badbdf7
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 06:28:31 +0500

    Add files via upload

---
+++ 4.FAMOUS QUOTE (2)/famousquote2.js
@@ -0,0 +1,4 @@
+"use strict";
+const famousperson = "Ibne Sina";
+const message = `${famousperson} once said, "width of life is more important than length of life."`;
+console.log(message);
---
+++ 4.FAMOUS QUOTE (2)/famousquote2.ts
@@ -0,0 +1,5 @@
+const famousperson: string ="Ibne Sina";
+
+const message: string = `${famousperson} once said, "width of life is more important than length of life."`;
+
+console.log(message);
