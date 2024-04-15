862dfbeacb4908aca8e107225081911a13fc63f7
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 06:35:21 +0500

    Add files via upload

---
+++ 11.NAMES/names.js
@@ -0,0 +1,7 @@
+"use strict";
+let friendsName = ["Afifa", "Ayesha", "Bushra", "Umar"];
+//index number   00        01         02       03
+console.log(friendsName[0]);
+console.log(friendsName[1]);
+console.log(friendsName[2]);
+console.log(friendsName[3]);
---
+++ 11.NAMES/names.ts
@@ -0,0 +1,8 @@
+let friendsName: string[] = ["Afifa", "Ayesha", "Bushra", "Umar"];
+             //index number   00        01         02       03
+                           
+
+console.log(friendsName[0]);
+console.log(friendsName[1]);
+console.log(friendsName[2]);
+console.log(friendsName[3]);
