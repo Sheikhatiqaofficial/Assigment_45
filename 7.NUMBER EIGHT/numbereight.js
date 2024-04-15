a6c4e8c81de06c4aabb633eeeccf83e742a48a56
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 06:32:07 +0500

    Add files via upload

---
+++ 7.NUMBER EIGHT/numbereight.js
@@ -0,0 +1,6 @@
+"use strict";
+const addition = 4 + 4;
+const subtraction = 10 - 2;
+const multiplication = 2 * 4;
+const division = 32 / 4;
+console.log(addition, subtraction, multiplication, division);
