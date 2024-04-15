dee016a4bda7429ff95a6973faceac24928a2114
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 06:46:01 +0500

    Add files via upload

---
+++ 12.GREETINGS/greetings.ts
@@ -0,0 +1,6 @@
+let friendsName: string[] =["Afifa", "Ayesha", "Bushra", "Umar"];
+
+console.log(`Hye ${friendsName[0]}, how's you doing?`);
+console.log(`Hye ${friendsName[1]}, how's you doing?`);
+console.log(`Hye ${friendsName[2]}, how's you doing?`);
+console.log(`Hye ${friendsName[3]}, how's you doing?`);
