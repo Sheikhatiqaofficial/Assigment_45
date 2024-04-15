8cadbe750b63f9e3eeb79d6f840169e1a370aba3
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 07:06:45 +0500

    Add files via upload

---
+++ 19.DINNER GUESTS/dinnerguests.ts
@@ -0,0 +1,8 @@
+let myFriends: string[] =["Afifa", "Ayesha", "Bushra", "Umar", "Adeel"];
+console.log(`i am inviting ${myFriends.length} of my friends in dinner at my villa which are:\n`)
+for(let i = 0; i < myFriends.length; i++){
+
+    console.log(`${i + 1}.${myFriends[i]}`)
+
+
+}
