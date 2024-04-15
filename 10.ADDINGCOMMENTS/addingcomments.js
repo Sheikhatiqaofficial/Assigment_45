d8de83949003ceb61c4b94cb96683a6aff02a408
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 06:34:23 +0500

    Add files via upload

---
+++ 10.ADDINGCOMMENTS/addingcomments.js
@@ -0,0 +1,5 @@
+"use strict";
+const num1 = 10089;
+const num2 = 4565;
+// now we will add num1 and num2(i am commenting out by pressing ctrl + /) :
+console.log(num1 + num2);
---
+++ 10.ADDINGCOMMENTS/addingcomments.ts
@@ -0,0 +1,6 @@
+const num1: number = 10089;
+
+const num2: number = 4565;
+
+// now we will add num1 and num2(i am commenting out by pressing ctrl + /) :
+console.log(num1 + num2);
