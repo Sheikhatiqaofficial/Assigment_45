5908c0a0643eb1173246bc5ce424c4bb99f2515d
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 06:52:29 +0500

    Add files via upload

---
+++ 13.YOUR OWN ARRAY/yourownarray.js
@@ -0,0 +1,5 @@
+"use strict";
+const myTrans = ["PRIVATE JET", "HELICOPTER", "BMW", "SPORTS BIKE"];
+for (let i = 0; i < myTrans.length; i++) {
+    console.log(`I would like to own ${myTrans[i]}`);
+}
---
+++ 13.YOUR OWN ARRAY/yourownarray.ts
@@ -0,0 +1,6 @@
+const myTrans : string [] =["PRIVATE JET", "HELICOPTER", "BMW", "SPORTS BIKE"];
+
+
+for(let i=0; i < myTrans.length; i++){
+       console.log(`I would like to own ${myTrans[i]}`);
+}
