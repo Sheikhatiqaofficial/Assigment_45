0f73405bed3dbf17f9265197d082eb96acba575c
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 06:27:37 +0500

    Add files via upload

---
+++ 3.FAMOUS QUOTE/famousquote.js
@@ -0,0 +1,3 @@
+"use strict";
+const myQuote = `Ibne Sina once said, "width of life is more important than length of life."`;
+console.log(myQuote);
---
+++ 3.FAMOUS QUOTE/famousquote.ts
@@ -0,0 +1,3 @@
+const myQuote: string = `Ibne Sina once said, "width of life is more important than length of life."`
+
+console.log(myQuote);
