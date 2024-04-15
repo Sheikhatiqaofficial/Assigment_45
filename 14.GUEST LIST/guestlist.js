7313974fabc0a9b2723d0327d9373b440e0cf632
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 07:01:17 +0500

    Add files via upload

---
+++ 14.GUEST LIST/guestlist.js
@@ -0,0 +1,5 @@
+"use strict";
+const myFriends = ["Afifa", "Ayesha", "Umar"];
+for (let i = 0; i < myFriends.length; i++) {
+    console.log(`Dear Mr/Miss. ${myFriends[i]}! you are invited for dinner at my villa.`);
+}
---
+++ 14.GUEST LIST/guestlist.ts
@@ -0,0 +1,6 @@
+const myFriends: string [] = ["Afifa", "Ayesha", "Umar"];
+
+for (let i = 0; i < myFriends.length; i++){
+ console.log(`Dear Mr/Miss. ${myFriends[i]}! you are invited for dinner at my villa.`);
+
+}
