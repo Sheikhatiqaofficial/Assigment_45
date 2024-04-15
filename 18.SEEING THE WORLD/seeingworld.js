257a6eaefc2e756747b8c7d2c4daa1d2bd55ccba
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 07:06:22 +0500

    Add files via upload

---
+++ 18.SEEING THE WORLD/seeingworld.js
@@ -0,0 +1,31 @@
+"use strict";
+let favPlaces = ["Netherland", "Paris", "Makkah", "Pakistan", "Japan"];
+console.log(favPlaces);
+//  Making a copy of an array:
+let copyofArray = favPlaces.slice();
+// Making a sorted of an array:
+let sortedArray = copyofArray.sort();
+console.log(sortedArray);
+// printing original array:
+console.log(favPlaces);
+// reversing original array:
+let reverseofarray = favPlaces.reverse();
+console.log(reverseofarray);
+// Again printing original array:
+console.log(favPlaces);
+// showing the original array:
+let coppyofarray2 = favPlaces.slice();
+let reverseofarray2 = coppyofarray2.reverse();
+console.log(reverseofarray2);
+// reversing the aray:
+let coppyofarray3 = favPlaces.reverse();
+console.log(coppyofarray3);
+let coppyofarray4 = favPlaces.reverse();
+console.log(coppyofarray4);
+let coppyofarray5 = favPlaces.reverse();
+console.log(coppyofarray5);
+// sorting it to get changed answer:
+let sortedArray2 = favPlaces.sort();
+console.log(sortedArray2);
+let reversesortedarray = sortedArray2.reverse();
+console.log(reversesortedarray);
